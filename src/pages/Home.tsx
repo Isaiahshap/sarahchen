40d99867940c675893f6211f9ef95da00ebaa471
Yeshaya <andyshapiro613@gmail.com>
2024-12-27 18:41:31 -0500

    s

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Footer from '../components/Footer'
-import { motion, useScroll } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
